a72cdc48ea9b99da97915908ac0fdf57af165453
Humza <hytrah@gmail.com>
2022-02-04 16:28:44 -0500

    working latest yt video (no thumbnail)

--- popup.js
+++ popup.js
@@ -5,11 +5,11 @@ const settingsBtn = document.getElementById("settingsBtn");
 const tweetLink = document.getElementById("twitterLink");
 
 chrome.storage.local.get("streamData", ({ streamData }) => {
-  const liveText = streamData.status === 'online' ? 'LIVE' : 'OFFLINE';
+  const liveText = streamData.status === "online" ? "LIVE" : "OFFLINE";
   statusText.innerText = `${liveText}`;
   streamText.innerText = `${trimTitle(streamData.title)}`;
 
-  if (streamData.status === 'offline') {
+  if (streamData.status === "offline") {
     const watchBtn = document.getElementById("watchButton");
     watchBtn.innerText = "Visit Channel";
   }
@@ -18,67 +18,81 @@ chrome.storage.local.get("streamData", ({ streamData }) => {
 window.onload = async () => {
   const streamInfo = await getStreamInfo();
   const imgUrl = await buildImageUrl(streamInfo[0].gameArt, 144, 192);
-  const sectionImage = document.getElementById('sectionImage');
+  const sectionImage = document.getElementById("sectionImage");
   sectionImage.src = imgUrl;
 
   const twitterPosts = await getTwitterPosts();
   tweetText.innerHTML = `Latest Tweet: </br> ${urlify(twitterPosts[0].text)}`;
   tweetLink.href = twitterPosts[0].url;
-}
+
+  const youtubeInfo = streamInfo[0];
+  const youtubeTitle = document.getElementById("youtubeTitle");
+  youtubeTitle.innerHTML = `Latest Video: </br> ${youtubeInfo.vidTitle}`;
+  const youtubeDescription = document.getElementById("youtubeDescription");
+  youtubeDescription.innerHTML = youtubeInfo.vidDesc;
+  const watchButtonYT = document.getElementById("watchButtonYT");
+  watchButtonYT.href = `https://youtube.com/watch?v=${youtubeInfo.vidId}`;
+};
 
 async function getStreamInfo() {
   const res = await fetch("https://api.onlyfands.net/info", {
-      method: 'GET',
-      headers: {
-          'content-type': 'application/json;'
-      },
-      mode: 'cors',
+    method: "GET",
+    headers: {
+      "content-type": "application/json;",
+    },
+    mode: "cors",
   });
 
-  const {Data} = await res.json();
+  const { Data } = await res.json();
   if (res.ok) {
-      return Data;
+    return Data;
   } else {
-      return Promise.reject('There was an error in retrieving the data.');
+    return Promise.reject("There was an error in retrieving the data.");
   }
 }
 
 async function getTwitterPosts() {
   const res = await fetch("https://api.onlyfands.net/posts", {
-      method: 'GET',
-      headers: {
-          'content-type': 'application/json;'
-      },
-      mode: 'cors',
+    method: "GET",
+    headers: {
+      "content-type": "application/json;",
+    },
+    mode: "cors",
   });
 
-  const {data} = await res.json();
+  const { data } = await res.json();
   if (res.ok) {
-      return data;
+    return data;
   } else {
-      return Promise.reject('There was an error in retrieving the data.');
+    return Promise.reject("There was an error in retrieving the data.");
   }
 }
 
 async function buildImageUrl(url, width, height) {
-  return url.replace('{width}', width).replace('{height}', height);
+  return url.replace("{width}", width).replace("{height}", height);
 }
 
 function trimTitle(title) {
-  return `${title.substring(0,75)}${title.length > 75 ? '...' : ''}`;
+  return `${title.substring(0, 75)}${title.length > 75 ? "..." : ""}`;
 }
 
 function urlify(text) {
   var urlRegex = /(https?:\/\/[^\s]+)/g;
-  return text.replace(urlRegex, function(url) {
-    return '<a style="color:#970e0f" href="' + url + '" target="_blank" rel="noopener noreferrer">' + url + '</a>';
-  })
+  return text.replace(urlRegex, function (url) {
+    return (
+      '<a style="color:#970e0f" href="' +
+      url +
+      '" target="_blank" rel="noopener noreferrer">' +
+      url +
+      "</a>"
+    );
+  });
 }
 
 settingsBtn.addEventListener("click", function () {
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
   } else {
-    window.open(chrome.runtime.getURL('options.html'));
+    window.open(chrome.runtime.getURL("options.html"));
   }
 });
